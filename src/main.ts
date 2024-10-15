6ff22b4095a74cc93d29e211dc4ec8150c0e368d
Uti Favour <utifavour15@gmail.com>
2024-10-15 11:55:21 +0100

    #Inavder Class Initialization

--- src/main.ts
+++ src/main.ts
@@ -7,44 +7,49 @@ canvas.style.background = 'black';
 document.body.appendChild(canvas);
 
 
+//Game variables
+let timeInterval = 100;
 
 
 class Shooter {
+    x: number;
+    y: number;
+
 
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
     }
 
-    x: number;
-    y: number;
 
 
     createImage() {
-
-        let right = 10, left = 10;
+        let position = 10;
 
         const img = new Image;
         img.addEventListener('load', () => {
-            ctx?.drawImage(img, right * this.x, left * this.y);
+            ctx?.drawImage(img, this.x, this.y);
         })
         img.src = 'public/shooter.png'
 
 
-        document.addEventListener('keydown' , (e) => {
-         switch (e.key) {
-            //Move up
-            case 'ArrowRight':
-                this.x++;
-                break;
+        document.addEventListener('keydown', (e) => {
+            switch (e.key) {
+                //Move up
+                case 'ArrowRight':
+                    this.x + position;
+                    console.log(this.x);
 
-            case 'ArrowLeft':
-                this.y++;
-                break;
-         
-            default:
-                break;
-         }
+                    break;
+
+                case 'ArrowLeft':
+                    this.y + position;
+                    console.log(this.y);
+                    break;
+
+                default:
+                    break;
+            }
         })
     }
 
@@ -55,10 +60,11 @@ class Shooter {
 
     //This releases bullets upon Arrow movement (For loop)
     releaseBullets() {
-            let bullet = new Image();
-            bullet.addEventListener('load' , ()=> {
-            ctx?.drawImage(bullet , 10, 10)
-            });
+        let bullet = new Image();
+        bullet.addEventListener('load', () => {
+            ctx?.drawImage(bullet, this.x, this.y - 40, 30, 30)
+        });
+        bullet.src = 'public/bullets.png'
     }
 
     //Checks collition of the invader and shooter
@@ -67,4 +73,38 @@ class Shooter {
     }
 }
 
+
+const shot = new Shooter(window.innerWidth / 2, window.innerHeight / 1.1);
+shot.createImage();
+shot.releaseBullets();
+
+
+//Logic for 
+class Invader {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    generateInvader(){
+        setInterval (() => {
+         let invader = new Image();
+         invader.addEventListener('load' , () => {
+            ctx?.drawImage(invader , this.x, this.y++)
+         })
+        invader.src = '/public/invader.png'
+         console.log('Hello World');
+        } , timeInterval);
+    }
+
+    
+}
+
+const initInader = new Invader(50, 50);
+
+
+initInader.generateInvader();
 
